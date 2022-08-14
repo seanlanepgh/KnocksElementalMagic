701b37bf074570bddeea7c16ce8f1c2b8889f3ff
seanlanepgh <seanlane997@gmail.com>
2022-08-14 11:37:44 +0100

    KnocksEntity Code

---
+++ src/main/java/io/github/seanlanepgh/knockselementalmagic/entity/KnocksEntity.java
@@ -0,0 +1,131 @@
+package io.github.seanlanepgh.knockselementalmagic.entity;
+
+import java.util.UUID;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.TimeUtil;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.NeutralMob;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.ai.navigation.WallClimberNavigation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.*;
+
+public class KnocksEntity extends PathfinderMob implements NeutralMob {
+	private static final EntityDataAccessor<Integer> ANGER_TIME = SynchedEntityData.defineId(KnocksEntity.class,
+			EntityDataSerializers.INT);
+	public static final EntityDataAccessor<Integer> STATE = SynchedEntityData.defineId(KnocksEntity.class,
+			EntityDataSerializers.INT);
+	private static final UniformInt ANGER_TIME_RANGE = TimeUtil.rangeOfSeconds(20, 39);
+	private UUID targetUuid;
+
+	protected KnocksEntity(EntityType<? extends PathfinderMob> type, Level worldIn) {
+		super(type, worldIn);
+		this.noCulling = false;
+		this.xpReward = (int) (this.getMaxHealth());
+	}
+
+
+	public static boolean passPeacefulAndYCheck(EntityType<? extends KnocksEntity> config, LevelAccessor world,
+												MobSpawnType reason, BlockPos pos, RandomSource random) {
+		if (world.getDifficulty() == Difficulty.PEACEFUL)
+			return false;
+		if ((reason != MobSpawnType.CHUNK_GENERATION && reason != MobSpawnType.NATURAL))
+			return !world.getBlockState(pos.below()).is(Blocks.NETHER_WART_BLOCK);
+		return !world.getBlockState(pos.below()).is(Blocks.NETHER_WART_BLOCK);
+	}
+
+	@SuppressWarnings("deprecation")
+	public boolean canStandOnFluid(Fluid p_230285_1_) {
+		return p_230285_1_.is(FluidTags.LAVA);
+	}
+
+	public int getAttckingState() {
+		return this.entityData.get(STATE);
+	}
+
+	public void setAttackingState(int time) {
+		this.entityData.set(STATE, time);
+	}
+
+	@Override
+	protected void defineSynchedData() {
+		super.defineSynchedData();
+		this.entityData.define(ANGER_TIME, 0);
+		this.entityData.define(STATE, 0);
+	}
+
+	@Override
+	public int getRemainingPersistentAngerTime() {
+		return this.entityData.get(ANGER_TIME);
+	}
+
+	@Override
+	public void setRemainingPersistentAngerTime(int time) {
+		this.entityData.set(ANGER_TIME, time);
+	}
+
+	@Override
+	public UUID getPersistentAngerTarget() {
+		return this.targetUuid;
+	}
+
+	@Override
+	public void setPersistentAngerTarget(UUID target) {
+		this.targetUuid = target;
+	}
+
+	@Override
+	public Packet<?> getAddEntityPacket() {
+		return NetworkHooks.getEntitySpawningPacket(this);
+	}
+
+	@Override
+	public void startPersistentAngerTimer() {
+		this.setRemainingPersistentAngerTime(ANGER_TIME_RANGE.sample(this.random));
+	}
+
+	@Override
+	protected boolean shouldDespawnInPeaceful() {
+		return true;
+	}
+
+	public void performRangedAttack(LivingEntity target, float pullProgress) {
+	}
+
+	@Override
+	protected float getSoundVolume() {
+		return 0.4F;
+	}
+
+	@Override
+	protected PathNavigation createNavigation(Level worldIn) {
+		return new WallClimberNavigation(this, worldIn);
+	}
+
+	@Override
+	public void playAmbientSound() {
+		SoundEvent soundevent = this.getAmbientSound();
+		if (soundevent != null) {
+			this.playSound(soundevent, 0.25F, this.getVoicePitch());
+		}
+
+	}
+
+}
