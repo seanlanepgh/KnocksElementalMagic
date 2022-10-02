2ccd332eb7b7b4170041efe538648a7cbfdf1afd
seanlanepgh <seanlane997@gmail.com>
2022-10-02 20:59:58 +0100

    Added Elemental Armor resistance effects

---
+++ src/main/java/io/github/seanlanepgh/knockselementalmagic/core/items/armor/effects/IceResistanceArmorEffect.java
@@ -0,0 +1,36 @@
+package io.github.seanlanepgh.knockselementalmagic.core.items.armor.effects;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class IceResistanceArmorEffect extends ArmorEffect {
+    private final float strength;
+
+    public IceResistanceArmorEffect(float strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public float applyArmorToDamage(LivingEntity entity, DamageSource source, float amount, ItemStack armor) {
+        if (source.isFire()) {
+            return amount * (1.0f - strength);
+        } else {
+            return amount;
+        }
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+
+        tooltip.add(Text.translatable("armorEffect.fireResistance", (int)(strength * 100)).formatted(Formatting.RED));
+    }
+}
