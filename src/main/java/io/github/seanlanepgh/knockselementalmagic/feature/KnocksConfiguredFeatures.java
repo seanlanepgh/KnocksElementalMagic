bde32f15b191a3ca243ed2b5c19b41272d783284
seanlanepgh <seanlane997@gmail.com>
2023-01-15 20:12:02 +0000

    Added Elemental Geodes,needs budding to be added and some experimenting with size, and different block layers

--- src/main/java/io/github/seanlanepgh/knockselementalmagic/feature/KnocksConfiguredFeatures.java
+++ src/main/java/io/github/seanlanepgh/knockselementalmagic/feature/KnocksConfiguredFeatures.java
@@ -6,9 +6,17 @@ import io.github.seanlanepgh.knockselementalmagic.KnocksElementalMagic;
 import io.github.seanlanepgh.knockselementalmagic.core.KnocksBlocks;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.GeodeBlockSettings;
+import net.minecraft.world.level.levelgen.GeodeCrackSettings;
+import net.minecraft.world.level.levelgen.GeodeLayerSettings;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.GeodeConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -41,6 +49,63 @@ public class KnocksConfiguredFeatures {
             () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_EARTH_CRYSTAL_ORES.get(),12)));
     public static final RegistryObject<ConfiguredFeature<?, ?>> OVERWORLD_AIR_CRYSTAL_ORE = CONFIGURED_FEATURES.register("air_crystal_ore",
             () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_AIR_CRYSTAL_ORES.get(),12)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> FIRE_CRYSTAL_GEODE = CONFIGURED_FEATURES.register("fire_crystal_geode",
+            () -> new ConfiguredFeature<>(Feature.GEODE,
+                    new GeodeConfiguration(new GeodeBlockSettings(BlockStateProvider.simple(Blocks.AIR),
+                            BlockStateProvider.simple(KnocksBlocks.FIRE_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(KnocksBlocks.FIRE_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(Blocks.NETHERRACK),
+                            BlockStateProvider.simple(Blocks.SMOOTH_BASALT),
+                            List.of(KnocksBlocks.FIRE_CRYSTAL_BLOCK.get().defaultBlockState()),
+                            BlockTags.FEATURES_CANNOT_REPLACE , BlockTags.GEODE_INVALID_BLOCKS),
+                            new GeodeLayerSettings(1.7D, 2.2D, 3.2D, 4.2D),
+                            new GeodeCrackSettings(0.95D, 2.0D, 2), 0.35D, 0.083D,
+                            true, UniformInt.of(4, 6),
+                            UniformInt.of(3, 4), UniformInt.of(1, 2),
+                            -16, 16, 0.05D, 1)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> ICE_CRYSTAL_GEODE = CONFIGURED_FEATURES.register("ice_crystal_geode",
+            () -> new ConfiguredFeature<>(Feature.GEODE,
+                    new GeodeConfiguration(new GeodeBlockSettings(BlockStateProvider.simple(Blocks.AIR),
+                            BlockStateProvider.simple(KnocksBlocks.ICE_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(KnocksBlocks.ICE_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(Blocks.PACKED_ICE),
+                            BlockStateProvider.simple(Blocks.SMOOTH_BASALT),
+                            List.of(KnocksBlocks.ICE_CRYSTAL_BLOCK.get().defaultBlockState()),
+                            BlockTags.FEATURES_CANNOT_REPLACE , BlockTags.GEODE_INVALID_BLOCKS),
+                            new GeodeLayerSettings(1.7D, 2.2D, 3.2D, 4.2D),
+                            new GeodeCrackSettings(0.95D, 2.0D, 2), 0.35D, 0.083D,
+                            true, UniformInt.of(4, 6),
+                            UniformInt.of(3, 4), UniformInt.of(1, 2),
+                            -16, 16, 0.05D, 1)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> EARTH_CRYSTAL_GEODE = CONFIGURED_FEATURES.register("earth_crystal_geode",
+            () -> new ConfiguredFeature<>(Feature.GEODE,
+                    new GeodeConfiguration(new GeodeBlockSettings(BlockStateProvider.simple(Blocks.AIR),
+                            BlockStateProvider.simple(KnocksBlocks.EARTH_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(KnocksBlocks.EARTH_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(Blocks.CALCITE),
+                            BlockStateProvider.simple(Blocks.SMOOTH_BASALT),
+                            List.of(KnocksBlocks.EARTH_CRYSTAL_BLOCK.get().defaultBlockState()),
+                            BlockTags.FEATURES_CANNOT_REPLACE , BlockTags.GEODE_INVALID_BLOCKS),
+                            new GeodeLayerSettings(1.7D, 2.2D, 3.2D, 4.2D),
+                            new GeodeCrackSettings(0.95D, 2.0D, 2), 0.35D, 0.083D,
+                            true, UniformInt.of(4, 6),
+                            UniformInt.of(3, 4), UniformInt.of(1, 2),
+                            -16, 16, 0.05D, 1)));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> AIR_CRYSTAL_GEODE = CONFIGURED_FEATURES.register("air_crystal_geode",
+            () -> new ConfiguredFeature<>(Feature.GEODE,
+                    new GeodeConfiguration(new GeodeBlockSettings(BlockStateProvider.simple(Blocks.AIR),
+                            BlockStateProvider.simple(KnocksBlocks.AIR_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(KnocksBlocks.AIR_CRYSTAL_BLOCK.get()),
+                            BlockStateProvider.simple(Blocks.CALCITE),
+                            BlockStateProvider.simple(Blocks.SMOOTH_BASALT),
+                            List.of(KnocksBlocks.AIR_CRYSTAL_BLOCK.get().defaultBlockState()),
+                            BlockTags.FEATURES_CANNOT_REPLACE , BlockTags.GEODE_INVALID_BLOCKS),
+                            new GeodeLayerSettings(1.7D, 2.2D, 3.2D, 4.2D),
+                            new GeodeCrackSettings(0.95D, 2.0D, 2), 0.35D, 0.083D,
+                            true, UniformInt.of(4, 6),
+                            UniformInt.of(3, 4), UniformInt.of(1, 2),
+                            -16, 16, 0.05D, 1)));
 
     public static void register(IEventBus eventBus) {
         CONFIGURED_FEATURES.register(eventBus);
--- src/main/java/io/github/seanlanepgh/knockselementalmagic/feature/KnocksPlacedFeatures.java
+++ src/main/java/io/github/seanlanepgh/knockselementalmagic/feature/KnocksPlacedFeatures.java
@@ -32,6 +32,27 @@ public class KnocksPlacedFeatures {
                     commonOrePlacement(12, // VeinsPerChunk
                             HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(256)))));
 
+    public static final RegistryObject<PlacedFeature> FIRE_CRYSTAL_GEODE_PLACED = PLACED_FEATURES.register("fire_crystal_geode_placed",
+            () -> new PlacedFeature(KnocksConfiguredFeatures.FIRE_CRYSTAL_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
+                    BiomeFilter.biome())));
+
+    public static final RegistryObject<PlacedFeature> ICE_CRYSTAL_GEODE_PLACED = PLACED_FEATURES.register("ice_crystal_geode_placed",
+            () -> new PlacedFeature(KnocksConfiguredFeatures.ICE_CRYSTAL_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
+                    BiomeFilter.biome())));
+    public static final RegistryObject<PlacedFeature> EARTH_CRYSTAL_GEODE_PLACED = PLACED_FEATURES.register("earth_crystal_geode_placed",
+            () -> new PlacedFeature(KnocksConfiguredFeatures.EARTH_CRYSTAL_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
+                    BiomeFilter.biome())));
+    public static final RegistryObject<PlacedFeature> AIR_CRYSTAL_GEODE_PLACED = PLACED_FEATURES.register("air_crystal_geode_placed",
+            () -> new PlacedFeature(KnocksConfiguredFeatures.AIR_CRYSTAL_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
+                    BiomeFilter.biome())));
     public static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
         return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
     }
