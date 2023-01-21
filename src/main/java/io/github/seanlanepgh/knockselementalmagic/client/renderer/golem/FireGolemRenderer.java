1a47e72e5e8cfe7e90d1e0ee6776bcb365de4fe9
seanlanepgh <seanlane997@gmail.com>
2023-01-21 15:42:19 +0000

    Added Nether Fire Golem Texture

--- src/main/java/io/github/seanlanepgh/knockselementalmagic/client/renderer/golem/FireGolemRenderer.java
+++ src/main/java/io/github/seanlanepgh/knockselementalmagic/client/renderer/golem/FireGolemRenderer.java
@@ -1,14 +1,15 @@
 package io.github.seanlanepgh.knockselementalmagic.client.renderer.golem;
+
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import io.github.seanlanepgh.knockselementalmagic.KnocksElementalMagic;
 import io.github.seanlanepgh.knockselementalmagic.client.model.golem.FireGolemModel;
 import io.github.seanlanepgh.knockselementalmagic.entity.golem.FireGolemEntity;
-import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class FireGolemRenderer extends GeoEntityRenderer<FireGolemEntity> {
@@ -19,8 +20,14 @@ public class FireGolemRenderer extends GeoEntityRenderer<FireGolemEntity> {
 
     @Override
     public ResourceLocation getTextureLocation(FireGolemEntity instance) {
+
+        if(instance.level.dimension() == Level.NETHER)
+        {
+            return new ResourceLocation(KnocksElementalMagic.MODID, "textures/entity/golem/nether_fire_golem.png");
+        }
         return new ResourceLocation(KnocksElementalMagic.MODID, "textures/entity/golem/fire_golem.png");
     }
+
 
     @Override
     public RenderType getRenderType(FireGolemEntity animatable, float partialTicks, PoseStack stack,
